727887aa9f5f01081b8db06bb14e28101d29caaf
Muhammed-Ata <mata724773@gmail.com>
2024-08-17 18:22:14 +0300

    gizli

--- NamazSite/script.js
+++ NamazSite/script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     playButton.addEventListener('click', () => {
         // Videoyu embed linkine ayarla
-        videoPlayer.src = 'https://www.youtube.com/watch?v=AuKR2fQbMBkautoplay.';
+        videoPlayer.src = 'https://www.youtube.com/watch?v=AuKR2fQbMBkautoplay1';
         // Video konteynerini görünür yap
         videoContainer.classList.remove('hidden');
     });
