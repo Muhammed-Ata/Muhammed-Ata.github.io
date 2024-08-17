c77897fca01ecec74906edc4d3a76ba9a05abb03
Muhammed-Ata <mata724773@gmail.com>
2024-08-17 18:18:04 +0300

    rickroll

--- NamazSite/script.js
+++ NamazSite/script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     playButton.addEventListener('click', () => {
         // Videoyu embed linkine ayarla
-        videoPlayer.src = 'https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1';
+        videoPlayer.src = 'https://www.youtube.com/watch?v=AuKR2fQbMBk';
         // Video konteynerini görünür yap
         videoContainer.classList.remove('hidden');
     });
