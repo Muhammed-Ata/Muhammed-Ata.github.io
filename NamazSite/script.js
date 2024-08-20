7e2f2b2cd2a3386944489b70611b25caaab99d77
Muhammed-Ata <mata724773@gmail.com>
2024-08-20 11:17:02 +0300

    ışık

--- NamazSite/script.js
+++ NamazSite/script.js
@@ -1,10 +1,4 @@
-document.getElementById('myButton').addEventListener('click', function() {
-    var videoContainer = document.getElementById('videoContainer');
-    var video = document.getElementById('video');
-
-    // YouTube video URL
-    video.src = 'https://www.youtube.com/embed/dQw4w9WgXcQ';
-
-    // Show the video container
-    videoContainer.style.display = 'block';
+document.getElementById('brightnessButton').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'white';
+    document.body.style.filter = 'brightness(10)';
 });
