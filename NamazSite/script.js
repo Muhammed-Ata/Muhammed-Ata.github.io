b2a7606decac278cb01ae725ae3795a1c3a86550
Muhammed-Ata <mata724773@gmail.com>
2024-08-20 11:18:40 +0300

    dad

--- NamazSite/script.js
+++ NamazSite/script.js
@@ -1,4 +1,4 @@
 document.getElementById('brightnessButton').addEventListener('click', function() {
-    document.body.style.backgroundColor = 'white';
+    document.body.style.backgroundColor = 'blue';
     document.body.style.filter = 'brightness(10)';
 });
