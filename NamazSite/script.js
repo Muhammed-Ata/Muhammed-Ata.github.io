56bdb11bf33f1f38f45ff40d296e34fe02a684a5
Muhammed-Ata <mata724773@gmail.com>
2024-12-07 08:43:49 +0300

    devri hümayun

--- NamazSite/script.js
+++ NamazSite/script.js
@@ -1,10 +1,18 @@
-document.getElementById("userInput").addEventListener("input", function() {
-    const input = this.value.toLowerCase();
-    const responseDiv = document.getElementById("response");
-    
-    if (input === "hello") {
-        responseDiv.textContent = "hello, how are you today";
-    } else {
-        responseDiv.textContent = "";
-    }
+// Harita oluştur
+var map = L.map('map').setView([39.92077, 32.85411], 6); // Ankara merkezli bir başlangıç noktası
+
+// Harita katmanı ekle
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+// Marker ekleme
+map.on('click', function (e) {
+    var lat = e.latlng.lat;
+    var lng = e.latlng.lng;
+
+    L.marker([lat, lng]).addTo(map)
+        .bindPopup(`Koordinatlar: ${lat.toFixed(5)}, ${lng.toFixed(5)}`)
+        .openPopup();
 });
